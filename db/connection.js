3fecb5a0488008df5b709c62d0a51f6a452473c2
Jackson Peak <jacksonpeak1@icloud.com>
2021-04-19 16:36:49 -0400

    Updated connection.js, scheema.sql, and seeds.sql files and linked to workbench

---
+++ db/connection.js
@@ -0,0 +1,25 @@
+const util = require("util");
+const mysql = require("mysql");
+
+// creates connection to sql database
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password:"Dolphin874!",
+    database: "employees_db"
+});
+
+connection.connect(function (err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+
+// Setting up connection.query to use promies instead of callbacks
+// This allows us to use teh async/await syntax
+connection.query = util.promisify(connection.query);
+
+module.exports = connection;
---
+++ db/index.js
@@ -0,0 +1,8 @@
+const connection = require("./connection");
+
+class DB {
+    // Keeping a refernce to the connection on the class in case we need it later
+    constructor(connection) {
+        this.connection = connection;
+    }
+}
